4aea5344f30b5f7574605d13766e69f17f50d69a
Vesa Meskanen <vesa.meskanen@cgi.com>
2023-03-09 14:28:07 +0200

    Temporary hack to filter bad vertices
    There is a bug in graph serialisation. Quick fix until the actual bug is fixed

--- src/main/java/org/opentripplanner/routing/linking/VertexLinker.java
+++ src/main/java/org/opentripplanner/routing/linking/VertexLinker.java
@@ -566,6 +566,9 @@ public class VertexLinker {
     int added = 0;
 
     for (IntersectionVertex v : visibilityVertices) {
+      if (!v.checkEdges()) {
+        continue;
+      }
       LineString newGeometry = GEOMETRY_FACTORY.createLineString(
         new Coordinate[] { nearestPoints[0], v.getCoordinate() }
       );
@@ -585,6 +588,9 @@ public class VertexLinker {
     // area calculation to be done after stop linking
     if (added == 0) {
       for (IntersectionVertex v : visibilityVertices) {
+        if (!v.checkEdges()) {
+          continue;
+        }
         createSegments(newVertex, v, edgeList, areas, scope, tempEdges);
       }
     }
--- src/main/java/org/opentripplanner/street/model/vertex/Vertex.java
+++ src/main/java/org/opentripplanner/street/model/vertex/Vertex.java
@@ -126,6 +126,13 @@ public abstract class Vertex implements AStarVertex<State, Edge, Vertex>, Serial
     return incoming.length;
   }
 
+  public boolean checkEdges() {
+    if (outgoing == null || incoming == null) {
+      return false;
+    }
+    return true;
+  }
+
   /** Get the longitude of the vertex */
   public final double getX() {
     return getLon();
@@ -193,13 +200,13 @@ public abstract class Vertex implements AStarVertex<State, Edge, Vertex>, Serial
    * Returns true if vertex is connected to another one by an edge
    */
   public boolean isConnected(Vertex v) {
-    for (Edge e : this.getOutgoing()) {
+    for (Edge e : outgoing) {
       if (e.getToVertex() == v) {
         return true;
       }
     }
-    for (Edge e : v.getOutgoing()) {
-      if (e.getToVertex() == this) {
+    for (Edge e : incoming) {
+      if (e.getFromVertex() == v) {
         return true;
       }
     }
