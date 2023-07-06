58d24c91fcdb19e86b2ca184cf303a4c5a331630
Vesa Meskanen <vesa.meskanen@cgi.com>
2023-07-06 11:19:00 +0300

    Add hard filter which compares walking
    Non-transit vs. transit filter now has a cost function, and silly itineraries
    where transit + walk has more walking as plain walk itinerary start appearing.
    Filter them away with dedicated logic.

--- src/main/java/org/opentripplanner/routing/algorithm/filterchain/deletionflagger/RemoveTransitIfStreetOnlyIsBetterFilter.java
+++ src/main/java/org/opentripplanner/routing/algorithm/filterchain/deletionflagger/RemoveTransitIfStreetOnlyIsBetterFilter.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.OptionalDouble;
 import java.util.stream.Collectors;
 import org.opentripplanner.model.plan.Itinerary;
+import org.opentripplanner.model.plan.Leg;
 
 /**
  * Filter itineraries based on generalizedCost, compared with a on-street-all-the-way itinerary(if
@@ -18,6 +19,14 @@ public class RemoveTransitIfStreetOnlyIsBetterFilter implements ItineraryDeletio
    * to know which filters removed
    */
   public static final String TAG = "transit-vs-street-filter";
+
+  private double getWalkDistance(Itinerary it) {
+    return it
+      .getStreetLegs()
+      .filter(l -> l.isWalkingLeg())
+      .mapToDouble(Leg::getDistanceMeters)
+      .sum();
+  }
 
   @Override
   public String name() {
@@ -35,14 +44,35 @@ public class RemoveTransitIfStreetOnlyIsBetterFilter implements ItineraryDeletio
     if (minStreetCost.isEmpty()) {
       return List.of();
     }
-
     final double limit = 60 + 1.3 * minStreetCost.getAsDouble();
 
-    // Filter away itineraries that have higher cost than limit cost computed above
-    return itineraries
+    // Filter away itineraries that have higher cost than limit cost
+    List<Itinerary> filtered = itineraries
       .stream()
       .filter(it -> !it.isOnStreetAllTheWay() && it.getGeneralizedCost() >= limit)
       .collect(Collectors.toList());
+
+    // Filter the most common silly itinerary case: transit itinerary has more walking than plain walk itinerary
+    OptionalDouble walkDistance = itineraries
+      .stream()
+      .filter(Itinerary::isWalkingAllTheWay)
+      .mapToDouble(Itinerary::distanceMeters)
+      .min();
+
+    if (walkDistance.isEmpty()) {
+      return filtered;
+    }
+    final double walkLimit = walkDistance.getAsDouble();
+    List<Itinerary> filtered2 = itineraries
+      .stream()
+      .filter(it -> getWalkDistance(it) > walkLimit)
+      .collect(Collectors.toList());
+
+    // remove duplicates
+    filtered2.removeAll(filtered);
+    filtered.addAll(filtered2);
+
+    return filtered;
   }
 
   @Override
