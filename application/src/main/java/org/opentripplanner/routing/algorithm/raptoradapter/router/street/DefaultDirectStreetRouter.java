03defce4557cf640b5423e2f944146edb36c6caa
Joel Lappalainen <lappalj8@gmail.com>
2025-11-24 20:15:47 +0200

    Simplify direct routing interface so that paths and itineraries are not lists

--- application/src/main/java/org/opentripplanner/routing/algorithm/raptoradapter/router/street/DefaultDirectStreetRouter.java
+++ application/src/main/java/org/opentripplanner/routing/algorithm/raptoradapter/router/street/DefaultDirectStreetRouter.java
@@ -1,6 +1,5 @@
 package org.opentripplanner.routing.algorithm.raptoradapter.router.street;
 
-import java.util.List;
 import org.opentripplanner.astar.model.GraphPath;
 import org.opentripplanner.framework.geometry.SphericalDistanceLibrary;
 import org.opentripplanner.routing.api.request.RouteRequest;
@@ -18,7 +17,7 @@ import org.opentripplanner.street.search.state.State;
  */
 public class DefaultDirectStreetRouter extends DirectStreetRouter {
 
-  List<GraphPath<State, Edge, Vertex>> findPaths(
+  GraphPath<State, Edge, Vertex> findPath(
     OtpServerRequestContext serverContext,
     LinkingContext linkingContext,
     RouteRequest request,
