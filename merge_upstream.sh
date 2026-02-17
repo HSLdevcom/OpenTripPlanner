215f99f0b9001b0765cef96a0f6211c70b7ae32d
Ville Pihlava <ville.pihlava@cgi.com>
2026-02-17 16:18:07 +0200

    Remove functionality related to rebasing from script.

--- merge_upstream.sh
+++ merge_upstream.sh
@@ -5,17 +5,14 @@
 
 # Capabilities of this script:
 # - Pull remote branch state (usually this should be the upstream OpenTripPlanner dev-2.x branch).
-# - CURRENTLY NOT IN USE - Rebase local extension branches on top of pulled upstream and push rebased extensions.
 # - Replace upstream CI actions with Digitransit setup.
 # - If configured, push the result (upstream + local modifications) to Digitransit OTP v2 (default) or custom-release branch.
 
-#set -euo pipefail
+set -euo pipefail
 
 DEVBRANCH=v2
 REMOTE_REPO=$(git remote -v | grep -i "hsldevcom/OpenTripPlanner" | grep "push" | awk '{print $1;}')
 DEFAULT_REMOTE=$(git config checkout.defaultRemote)
-STATUS_FILE=".merge_upstream.tmp"
-STATUS=""
 OTP_BASE=""
 FORCE_PUSH_TO_BRANCH=""
 CUSTOM_RELEASE=""
@@ -45,10 +42,7 @@ echo_bold() {
 
 function main() {
   setup "$@"
-  resumePreviousExecution
   resetDevelop
-  # digitransit currently has no extension features
-  # rebaseAndMergeExtBranch digitransit_ext_features
   configDigitransitCI
   logSuccess
 }
@@ -113,32 +107,6 @@ function setup() {
   git fetch --all
 }
 
-# This script create a status file '.merge_upstream.tmp'. This file is used to resume the
-# script in the same spot as where is left when the error occurred. This allow us to fix the
-# problem (merge conflict or compile error) and re-run the script to complete the proses.
-function resumePreviousExecution() {
-  readStatus
-
-  if [[ -n "${STATUS}" ]]; then
-    echo ""
-    echo "Resume: ${STATUS}?"
-    echo ""
-    echo "    If all problems are resolved you may continue."
-    echo "    Exit to clear status and start over."
-    echo ""
-
-    ANSWER=""
-    while [[ ! "$ANSWER" =~ [yx] ]]; do
-      echo "Do you want to resume: [y:Yes, x:Exit]"
-      read ANSWER
-    done
-
-    if [[ "${ANSWER}" == "x" ]]; then
-      exit 0
-    fi
-  fi
-}
-
 function resetDevelop() {
   echo ""
   echo_bold "## ------------------------------------------------------------------------------------- ##"
@@ -150,84 +118,14 @@ function resetDevelop() {
 
   whatDoYouWant
 
-  if [[ "${ANSWER}" == "y" ]]; then
-    echo ""
-    echo "Checkout '${DEVBRANCH}'"
-    git checkout ${DEVBRANCH}
-
-    echo ""
-    echo "Reset '${DEVBRANCH}' branch to '${OTP_BASE}' (hard)"
-    git reset --hard "${OTP_BASE}"
-    echo ""
-  fi
-}
-
-function rebaseAndMergeExtBranch() {
-  EXT_BRANCH="$1"
-  EXT_STATUS_REBASE="Rebase '${EXT_BRANCH}'"
-  EXT_STATUS_COMPILE="Compile '${EXT_BRANCH}'"
+  echo ""
+  echo "Checkout '${DEVBRANCH}'"
+  git checkout ${DEVBRANCH}
 
   echo ""
-  echo_bold "## ------------------------------------------------------------------------------------- ##"
-  echo_bold "##   REBASE AND MERGE '${EXT_BRANCH}' INTO '${DEVBRANCH}'"
-  echo_bold "## ------------------------------------------------------------------------------------- ##"
+  echo "Reset '${DEVBRANCH}' branch to '${OTP_BASE}' (hard)"
+  git reset --hard "${OTP_BASE}"
   echo ""
-  echo "You are about to rebase and merge '${EXT_BRANCH}' into '${DEVBRANCH}'. Any local"
-  echo "modification in the '${EXT_BRANCH}' will be lost."
-  echo ""
-
-  whatDoYouWant
-
-  if [[ "${ANSWER}" == "y" ]]; then
-    echo ""
-    echo "Checkout '${EXT_BRANCH}'"
-    git checkout "${EXT_BRANCH}"
-
-    echo ""
-    echo "Reset to '${REMOTE_REPO}/${EXT_BRANCH}'"
-    git reset --hard "${REMOTE_REPO}/${EXT_BRANCH}"
-
-    echo ""
-    echo "Top 2 commits in '${EXT_BRANCH}'"
-    echo "-------------------------------------------------------------------------------------------"
-    git --no-pager log -2
-    echo "-------------------------------------------------------------------------------------------"
-    echo ""
-    echo "You are about to rebase the TOP COMMIT ONLY(see above). Check that the "
-    echo "'${EXT_BRANCH}' only have ONE commit that you want to keep."
-    echo ""
-
-    whatDoYouWant
-
-    if [[ "${ANSWER}" == "y" ]]; then
-      echo ""
-      echo "Rebase '${EXT_BRANCH}' onto '${DEVBRANCH}'"
-      setStatus "${EXT_STATUS_REBASE}"
-      git rebase --onto ${DEVBRANCH} HEAD~1
-    fi
-  fi
-
-  if [[ "${STATUS}" == "${EXT_STATUS_REBASE}" || "${STATUS}" == "${EXT_STATUS_COMPILE}" ]]; then
-    # Reset status in case the test-compile fails. We need to do this because the status file
-    # is deleted after reading the status in the setup() function.
-    setStatus "${EXT_STATUS_COMPILE}"
-
-    mvn clean test-compile
-    clearStatus
-
-    echo ""
-    echo "Push '${EXT_BRANCH}'"
-    if [[ -n "${FORCE_PUSH_TO_BRANCH}" ]]; then
-      git push -f
-    else
-      echo "Skip: git push -f   (--dryRun)"
-    fi
-
-    echo ""
-    echo "Checkout '${DEVBRANCH}' and merge in '${EXT_BRANCH}'"
-    git checkout "${DEVBRANCH}"
-    git merge "${EXT_BRANCH}"
-  fi
 }
 
 function configDigitransitCI() {
@@ -236,6 +134,8 @@ function configDigitransitCI() {
   git checkout origin/digitransit_ext_config .github
   git commit -a -m "Configure Digitransit CI actions"
   if [[ -n "${FORCE_PUSH_TO_BRANCH}" ]]; then
+    echo ""
+    echo "Force pushing contents to '${DEVBRANCH}'"
     git push -f
   fi
 }
@@ -255,44 +155,18 @@ function whatDoYouWant() {
   echo ""
   ANSWER=""
 
-  if [[ -n "${STATUS}" ]]; then
-    # Skip until process is resumed
-    ANSWER="s"
-  else
-    while [[ ! "$ANSWER" =~ [ysx] ]]; do
-      echo_yellow "Do you want to continue: [y:Yes, s:Skip, x:Exit]"
-      read -r ANSWER
-    done
+  read -r -p "Do you want to continue: [y:Yes or n:No]?" ANSWER
 
-    if [[ "${ANSWER}" == "x" ]]; then
-      exit 0
-    fi
+  # If the answer isn't yes, then exit the script.
+  if [[ ! "${ANSWER,,}" =~ ^(y|yes)$ ]]; then
+    exit 0
   fi
-}
-
-function setStatus() {
-  STATUS="$1"
-  echo "$STATUS" >"${STATUS_FILE}"
-}
-
-function readStatus() {
-  if [[ -f "${STATUS_FILE}" ]]; then
-    STATUS=$(cat $STATUS_FILE)
-    rm "$STATUS_FILE"
-  else
-    STATUS=""
-  fi
-}
-
-function clearStatus() {
-  STATUS=""
-  rm "${STATUS_FILE}"
 }
 
 function printHelp() {
   echo ""
   echo -e "This script requires one argument, the base ${BOLD}branch${BASH_ENDFORMAT} or ${BOLD}commit${BASH_ENDFORMAT} to use for the output branch."
-  echo "The output branch v2 (default) or custom-release is reset to this commit and then the extension branches are rebased onto that."
+  echo "The output branch, v2 (default) or custom-release, is reset to this commit."
   echo "Changes are force pushed to the remote git repo if the -p flag is used."
   echo ""
   echo "Make sure origin is set to hsldevcom/OpenTripPlanner and that it is the default repository (git config checkout.defaultRemote origin)."
@@ -303,7 +177,7 @@ function printHelp() {
   echo "  MANDATORY:"
   echo "    -b : The base branch (or pathspec) to use for the output. Given as an argument to 'git reset --hard <argument>'."
   echo "  OPTIONAL:"
-  echo "    -p : Force push to the selected branch v2 (default) or custom-release at the end of the script."
+  echo "    -p : Force push to the selected branch, v2 (default) or custom-release, at the end of the script."
   echo "    -c : Use the custom-release branch instead of v2 for the output of this script."
   echo ""
   echo_bold "Usage:"
